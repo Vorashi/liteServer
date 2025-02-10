a4811b33cb13cd60c44536195d7e8962736d1da5
Vorashi <126412852+Vorashi@users.noreply.github.com>
2025-02-10 11:57:08 +0300

    next

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server start runing on ${PORT}!`)
+app.listen(3000, () => {
+    console.log(`Server start runing on 3000!`)
 });
